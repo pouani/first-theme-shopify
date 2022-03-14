12ebd9eb8fdcde20b391fa79dcae3df1a9a821ce
unknown <davidpouani228@gmail.com>
2022-03-14 19:08:49 +0100

    creation repertoire section, snippets avec header et footer

---
+++ assets/theme.js
@@ -0,0 +1,19 @@
+// Put your application javascript here
+
+// fonction douverture et fermeture du menu en forma mobile
+function OpenMenu() {
+    const menu = document.querySelector('.link');
+    const icon = document.querySelector(".icon");
+    if(icon.classList.contains("fa-bars")){
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+        menu.style.display = "block";
+    }else{
+        icon.classList.add("fa-bars");
+        icon.classList.remove("fa-xmark");
+        menu.style.display = "none";
+    }
+
+    
+}
+// fin fonction
